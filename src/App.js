0f08d0f28b7cc3cd9ce313e54b04db873a1ee624
Ramesh Gautam <ramesh@naphix.com.au>
2021-03-23 10:28:44 +0545

    Create fresh component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+	return(
+		<div className="title__holder">
+			<h1>Hello world</h1>
+			<h2>Second Title</h2>
+		</div>
+	);
+}
+
+export default App;
