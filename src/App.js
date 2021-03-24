5933b5456009fa37d787a85872c795ff65aab553
Ramesh Gautam <ramesh@naphix.com.au>
2021-03-24 14:15:08 +0545

    Update time refresh and click button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import Select from 'react-select';
+
 
 function App() {
+	// const state = useState();
+	// current data is count
+	// updated data setCount
+	// useState is a hook
+	// const [ count, setCount ] = useState(0);
+
+	// let curDate = new Date();
+	let newTime = new Date().toLocaleTimeString();
+
+	const [ ctime, setCtime ] = useState(newTime);
+
+	// method IncNum
+	const RefreshTime = () => {
+		// setCount(count + 1);
+		let cnewTime = new Date().toLocaleTimeString();
+		setCtime( cnewTime );
+	}
+
+	// method DecNum
+	// const DecNum = () => {
+	// 	setCount(count - 1);
+	// 	console.log(this);
+
+	// 	if( count == 0 ) {
+	// 		setCount(0);
+	// 	}
+	// }
+
 	return(
-		<div className="title__holder">
-			<h1>Hello world</h1>
-			<h2>Second Title</h2>
-		</div>
+		<React.Fragment>
+			<h1> { ctime } </h1>
+			<button onClick = { RefreshTime }>Get time</button>
+			{/* <button onClick = { IncNum }>Increment Num</button> */}
+		</React.Fragment>
 	);
 }
 
